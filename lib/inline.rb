2be6aec457f34bccf9bdda4191b42e047d947979
Ryan Davis <ryand-ruby@zenspider.com>
2022-05-23 10:25:04 -0800

    prepping for release [git-p4: depot-paths = "//src/RubyInline/dev/": change = 13405]

--- lib/inline.rb
+++ lib/inline.rb
@@ -65,7 +65,7 @@ class CompilationError < RuntimeError; end
 # the current namespace.
 
 module Inline
-  VERSION = "3.12.5"
+  VERSION = "3.12.6"
 
   WINDOZE  = /mswin|mingw/ =~ RUBY_PLATFORM
   DEV_NULL = (WINDOZE ? 'nul'      : '/dev/null')
